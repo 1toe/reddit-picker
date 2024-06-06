500a1a52423f2ce93fa2777b839027a870882f67
Wal-Wandering <72948318+BNegro1@users.noreply.github.com>
2024-06-06 19:11:18 -0400

    Actualizar script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ const postBox = document.getElementById('postBox');
 const toggleThemeBtn = document.getElementById('toggleTheme');
 const themeIcon = toggleThemeBtn.querySelector('i');
 
-document.getElementById('fetchAskReddit').addEventListener('click', async () => {
-  const subreddit = 'askreddit';
+const fetchPost = async (subreddit) => {
   const response = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
   const data = await response.json();
   const post = data[0].data.children[0].data;
@@ -13,34 +12,22 @@ document.getElementById('fetchAskReddit').addEventListener('click', async () => 
   document.getElementById('postLink').href = `https://www.reddit.com${post.permalink}`;
   
   postBox.classList.remove('d-none');
-});
+};
 
-document.getElementById('fetchAskRedditEspanol').addEventListener('click', async () => {
-  const subreddit = 'AskRedditEspanol'; 
-  const response = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
-  const data = await response.json();
-  const post = data[0].data.children[0].data;
-
-  document.getElementById('postTitle').textContent = post.title;
-  document.getElementById('postContent').textContent = post.selftext || "Click 'Read More' to view the full post.";
-  document.getElementById('postLink').href = `https://www.reddit.com${post.permalink}`;
-  
-  postBox.classList.remove('d-none');
-});
+document.getElementById('fetchAskReddit').addEventListener('click', () => fetchPost('askreddit'));
+document.getElementById('fetchAskRedditEspanol').addEventListener('click', () => fetchPost('AskRedditEspanol'));
 
 document.getElementById('changeLang').addEventListener('click', () => {
   const currentLang = document.documentElement.lang;
 
-  // Intercambio de idiomas
   if (currentLang === 'es') {
     document.documentElement.lang = 'en';
     document.getElementById('title').textContent = 'r/Reddit-Picker';
     document.getElementById('fetchAskReddit').textContent = 'r/AskReddit';
-    document.getElementById('fetchAskRedditEspanol').textContent = 'r/AskRedditEspanol';
+    document.getElementById('fetchAskRedditEspanol').textContent = 'r/AskRedditEspañol';
     document.getElementById('changeLang').textContent = 'Change to Spanish';
     document.getElementById('postContent').textContent = 'Click "Read More" to view the full post.';
     document.getElementById('postLink').textContent = 'Read More';
-
   } else {
     document.documentElement.lang = 'es';
     document.getElementById('title').textContent = 'r/Reddit-Picker';
@@ -52,14 +39,12 @@ document.getElementById('changeLang').addEventListener('click', () => {
   }
 });
 
-// Aplicar color en base al usuariop
 const applyTheme = (theme) => {
   document.body.classList.remove('light', 'dark');
   document.body.classList.add(theme);
   themeIcon.className = theme === 'dark' ? 'fas fa-moon' : 'fas fa-sun';
 };
 
-// Boton de alternado
 toggleThemeBtn.addEventListener('click', () => {
   const currentTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
   const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
